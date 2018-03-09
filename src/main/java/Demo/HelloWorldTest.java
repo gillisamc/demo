47d4b9b13fd394575c737033d9a2dcb53851eef9
girishmc <girishmc5@gmail.com>
2018-03-09 01:23:19 -0600

    added test

---
+++ src/main/java/Demo/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+	@Test
+	public void testHelloWorld1() {
+		HelloWorld hello = new HelloWorld();
+		
+		Assert.assertEquals(HelloWorld.HelloWorld1(), "Hello World1");
+		
+	}
+
+}
